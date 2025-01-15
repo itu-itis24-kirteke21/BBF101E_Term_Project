66c8b5cd454e042ca5eb86a3cb5354c860b98423
itu-itis24-kirteke21 <kirteke21@itu.edu.tr>
2025-01-15 18:18:25 +0300

    Progresses are made.Now it's time to work for Final.

--- Word_Prediction_Game/script.js
+++ Word_Prediction_Game/script.js
@@ -1,103 +1,95 @@
 const challengeInput = document.getElementById("challenge");
 const userInput = document.getElementById("user-input");
-const timerElement = document.getElementById("timer");
 const messageElement = document.getElementById("message");
-const levelElement = document.getElementById("level");
 const restartBtn = document.getElementById("restart-btn");
 const submitBtn = document.getElementById("submit-btn");
+const B = document.getElementById("b")
+const B_image = document.getElementById("B")
+const L = document.getElementById("l")
+const L_image = document.getElementById("L")
+const A = document.getElementById("a")
+const A_image = document.getElementById("A")
+const S = document.getElementById("s")
+const S_image = document.getElementById("S")
+const T = document.getElementById("t")
+const T_image = document.getElementById("T")
 
-const months = ["January", "February", "March", "April", "May", "June", "July",
-"August", "September", "October", "November", "December"];
-const numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight",
-"nine", "ten"];
-const colors = ["red", "blue", "green", "yellow", "purple",
-"orange","black","white"]
-
-
-let level = 1;
-let challengeText = "";
-let timeLeft = 45;
-let timerInterval;
+function endGame(){
+        messageElement.textContent ="Congratulations! You won the game.";
+        messageElement.style.color ='green';
+        alert("You have won the game")
+    restartBtn.classList.remove("hidden");
+}
+let B_flag =0
+let L_flag =0
+let A_flag =0
+let S_flag =0
+let T_flag =0
 
 function startGame() {
-    level =  1;
-    timeLeft = 45;
-    levelElement.textContent = level;
     restartBtn.classList.add("hidden");
     submitBtn.classList.remove("hidden");
     userInput.value = "";
     messageElement.textContent = "";
-    setChallengeText();
-    startTimer();
-}
-
-function setChallengeText(){
-    if (level == 1){
-        challengeText = getRandomElement(months);
-    }else if (level == 2){
-        challengeText = getRandomElement(months) + getRandomElement(numbers);
-    }else if (level == 3){
-        challengeText = getRandomElement(months) + getRandomElement(numbers) + getRandomElement(colors);
-    }
-    challengeInput.value = challengeText;
-}
-
-function getRandomElement(array){
-    return array[Math.floor(Math.random() * array.length)];
-}
-
-function startTimer(){
-    clearInterval(timerInterval);
-    timerInterval = setInterval(() =>{
-        timeLeft--;
-        timerElement.textContent = timeLeft;
-        if (timeLeft <= 0){
-            clearInterval(timerInterval);
-            endGame(false);
-        }
-    },1000);// 1000 ms,set intervalin 2.inputu milisaniye cinsinden zaman.
-}
-
-function endGame(won){
-    clearInterval(timerInterval);
-    if(won){
-        messageElement.textContent ="Congratulations! You won the game.";
-        messageElement.style.color ='green';
-    } else {
-        messageElement.textContent = "Time is up! You lost the game.";
-        messageElement.style.color = 'red';
-    }
-    restartBtn.classList.remove("hidden");
-    submitBtn.classList.add("hidden");
+    B.classList.remove("hidden");
+    B_image.classList.add("hidden")
+    L.classList.remove("hidden");
+    L_image.classList.add("hidden")
+    A.classList.remove("hidden");
+    A_image.classList.add("hidden")
+    S.classList.remove("hidden");
+    S_image.classList.add("hidden")
+    T.classList.remove("hidden");
+    T_image.classList.add("hidden")
+    B_flag = 0;
+    L_flag = 0;
+    A_flag = 0;
+    S_flag = 0;
+    T_flag = 0;
 }
 
 submitBtn.addEventListener("click", () => {
-    const reversedText = userInput.value;
-    const correctReversedText = challengeText.split("").reverse().join("");
-    if(reversedText == correctReversedText) {
-        if (level == 2){
-            level++;
-            timeLeft = 45;//resetting timer for next level
-            levelElement.textContent = level;
-            messageElement.textContent = "Correct!Keep moving to the next step."
-            messageElement.style.color = 'green';
-            userInput.value = "";
-            setChallengeText();
-        } else if (level ==1) {
-            level++;
-            timeLeft = 45;//resetting timer for next level
-            levelElement.textContent = level;
-            messageElement.textContent = "Correct!Keep moving to the next step."
-            messageElement.style.color = 'green';
-            userInput.value = "";
-            setChallengeText();
-        } else if (level ==3) {
-            endGame(true);
-        }
-
-    } else {
+    const Input_val = userInput.value.toUpperCase();
+    if((Input_val == "B") && (B_flag == 0)) {
+        B.classList.add("hidden");
+        B_image.classList.remove("hidden")
+        messageElement.textContent = "Nice guess!";
+        messageElement.style.color ='green';
+        B_flag =1;
+    }else if((Input_val == "L") && (L_flag == 0)) {
+        L.classList.add("hidden");
+        L_image.classList.remove("hidden")
+        messageElement.textContent = "Nice guess!";
+        messageElement.style.color ='green';
+        L_flag =1;
+    }else if((Input_val == "A") && (A_flag == 0)) {
+        A.classList.add("hidden");
+        A_image.classList.remove("hidden")
+        messageElement.textContent = "Nice guess!";
+        messageElement.style.color ='green';
+        A_flag =1;
+    }else if((Input_val == "S") && (S_flag == 0)) {
+        S.classList.add("hidden");
+        S_image.classList.remove("hidden")
+        messageElement.textContent = "Nice guess!";
+        messageElement.style.color ='green';
+        S_flag =1;
+    }else if((Input_val == "T") && (T_flag == 0)) {
+        T.classList.add("hidden");
+        T_image.classList.remove("hidden")
+        messageElement.textContent = "Nice guess!";
+        messageElement.style.color ='green'
+        T_flag =1;
+    }
+     else {
         messageElement.textContent = "Incorrect! Try again.";
         messageElement.style.color ='red';
+    }
+    let Total_flag = B_flag + L_flag + A_flag + S_flag + T_flag
+    if(Total_flag ==5) {
+        setTimeout(() => {
+            endGame();
+        }, 100);
     }
 });
 
